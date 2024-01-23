937241fab1c583f67951664ca22ac5c45a6e0830
Hamid <hemidbeyli2020@gmail.com>
2024-01-23 03:43:42 -0800

    added args function

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,14 @@
 if __name__ == "__main__":
 
     import sys
-    args = sys.argv
-    if len(args) > 1:
-        print('{} arguments:'.format(len(args) - 1))
-        for i in range(1, len(args)):
-            print('{}: {}'.format(i, args[i]))
+    
+    count = len(sys.argv) - 1
+    
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print('0 arguments.')
+        print("{} arguments:".format(count))
+    for i in range(1, count):
+        print("{}: {}".format(i, sys.argv[i]))
