e1db349567c2dc14367050d0fdfb3fecefcfcb96
Hamid <hemidbeyli2020@gmail.com>
2024-01-23 08:05:42 -0800

    Added add_tuple function

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,6 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     a = (tuple_a + (0, 0))[:2]
     b = (tuple_b + (0, 0))[:2]
-    new_tuple = (a[0] + b[0], a[1] + b[1])
+    
+    return (a[0] + b[0], a[1] + b[1])
 
-    return(new_tuple)
