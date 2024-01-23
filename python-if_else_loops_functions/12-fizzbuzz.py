b85570bc8a0a8ab54a38476a2dd8a1d5885334ca
Hamid <hemidbeyli2020@gmail.com>
2024-01-23 02:44:40 -0800

    Bug fixed fizzbuzz function

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 
 def fizzbuzz():
     for i in range(101):
-        if i % 3 and i % 5:
+        if i % 15 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
-            print(i, end="")
+            print(i, end=" ")
