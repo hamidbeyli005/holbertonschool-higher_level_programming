39a950f131ca415036aea8fe944e6c3c1d61cacc
Hamid <hemidbeyli2020@gmail.com>
2024-01-30 04:33:18 -0800

    Updated search_replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    new_list[new_list.index(search)] = replace
+    new_list= [replace if item == search else item for item in my_list]
     return new_list
