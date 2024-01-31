d2ce93d500f97062daf30bf34e42e0198edfc08c
Hamid <hemidbeyli2020@gmail.com>
2024-01-31 06:10:28 -0800

    Updated

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,22 +2,14 @@
 """
 Module to sum
 add_integer: adds two integers and return
+
 """
 
 
 def add_integer(a, b=98):
 
     """Adds two integers.
-
-    Args:
-        a (int or float): The first number.
-        b (int or float): The second number (default is 98).
-
-    Returns:
-        int: The addition of a and b.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
+    Args: a = int/float b = int/float
     """
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
