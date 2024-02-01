2afc26388364c3e6696650fe162f7dae3bbe243f
Hamid <hemidbeyli2020@gmail.com>
2024-02-01 06:15:26 -0800

    Updated

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
-def say_my_name(first_name, last_name=""):
+"""
+my_name_module
 
+This module provides a function for printing a formatted name.
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        Print a formatted name.
+
+        Parameters:
+            first_name (str): The first name.
+            last_name (str): The last name. Defaults to an empty string.
+
+        Raises:
+            TypeError: If first_name is not a string.
+            TypeError: If last_name is not a string.
+    """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
