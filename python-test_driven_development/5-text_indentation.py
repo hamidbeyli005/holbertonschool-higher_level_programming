7a70ff9439ba309741153a6187e3b26d92de1cf6
Hamid <hemidbeyli2020@gmail.com>
2024-02-02 03:48:02 -0800

    Updated

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+"""
+Module: 5-text_indentation
+
+Function:
+    text_indentation(text)
+
+Description:
+    Formats input text by printing two new lines after each '.', '?', or ':'.
+
+Usage Example:
+    text_indentation("Lorem ipsum dolor sit amet. Consectetur adipiscing elit.")
+"""
+
+
 def text_indentation(text):
+    """
+    Args:
+        text (str): Input text to be formatted.
+
+    Raises:
+        TypeError: If input is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
