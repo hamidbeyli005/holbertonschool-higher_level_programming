730ba242f41272105ff88beca0eb7f482d576bee
Hamid <hemidbeyli2020@gmail.com>
2024-02-04 06:40:31 -0800

    Bug fixed

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -37,8 +37,11 @@ def matrix_mul(m_a, m_b):
     if any(not isinstance(element, (int, float)) for row in m_b for element in row):
         raise TypeError("m_b should contain only integers or floats")
 
-    if any(len(row) != len(m_a[0]) for row in m_a[1:]) or any(len(row) != len(m_b[0]) for row in m_b[1:]):
-        raise ValueError("each row of m_a must be of the same size")
+    if any(len(row) != len(m_a[0]) for row in m_a[1:]):
+    raise ValueError("each row of m_a must be of the same size")
+
+    if any(len(row) != len(m_b[0]) for row in m_b[1:]):
+        raise ValueError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
