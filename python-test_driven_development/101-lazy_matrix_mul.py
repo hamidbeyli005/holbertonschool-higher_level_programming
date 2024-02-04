8d2e26610299826e68b55f9eac095b9239adb82b
Hamid <hemidbeyli2020@gmail.com>
2024-02-04 08:37:10 -0800

    Updated 990

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -3,11 +3,18 @@
 This is the module 101-lazy_matrix_mul.
 It contains one function: 101-lazy_matrix_mul
 """
-import numpy
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """
     returns the product of m_a and m_b
     """
-    return numpy.matmul(m_a, m_b)
+    try:
+        result = np.matmul(m_a, m_b)
+    except ValueError as e:
+        if "Scalar operands are not allowed" in str(e):
+            raise ValueError("Scalar operands are not allowed, use '*' instead")
+        else:
+            raise
+    return result
