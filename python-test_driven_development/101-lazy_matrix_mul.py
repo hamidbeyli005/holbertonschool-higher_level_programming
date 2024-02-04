221a13f36e63a678d58c68e1506d9a60e65874f3
Hamid <hemidbeyli2020@gmail.com>
2024-02-04 08:50:15 -0800

    Updated

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,37 +5,64 @@ It contains one function: 101-lazy_matrix_mul
 """
 
 
-import numpy as np
-def lazy_matrix_mul(m_a, m_b):
-    '''
-    Function multiplies two matrices with numpy
-    '''
-    if isinstance(m_a, list) is False:
-        raise TypeError('m_a must be a list')
-    if isinstance(m_b, list) is False:
-        raise TypeError('m_b must be a list')
-    if not (isinstance(item, list) for item in m_a):
-        raise TypeError('m_a must be a list of lists')
-    if not (isinstance(item, list) for item in m_b):
-        raise TypeError('m_b must be a list of lists')
-    if not m_a:
-        raise ValueError("m_a can't be empty")
-    if not m_b:
-        raise ValueError("m_b can't be empty")
-    if not all(isinstance(i, float) or isinstance(i, int)
-            for l in m_a for i in l):
-        raise TypeError('m_a should contain only integers or floats')
-    if not all(isinstance(i, float) or isinstance(i, int)
-               for l in m_b for i in l):
-        raise TypeError('m_b should contain only integers or floats')
-    if not all(len(i) == len(m_a[0]) for i in m_a):
-        raise TypeError('each row of m_a must should be of the same size')
-    if not all(len(i) == len(m_b[0]) for i in m_b):
-        raise TypeError('each row of m_b must should be of the same size')
-    if len(m_a[0]) != len(m_b):
-        raise ValueError('m_a and m_b can\'t be multiplied')
+import numpy
 
-    a = np.array(m_a)
-    b = np.array(m_b)
-    c = np.dot(a, b)
-    return c
+
+def lazy_matrix_mul(m_a, m_b):
+    """mul m_a by m_b
+    Args:
+        m_a: the first matrix
+        m_b: the other matrix
+    Returns:
+        matrix: product
+    Raises:
+        TypeError: if m_a or m_b are not lists or not lists of lists
+        ValueError: if m_a or m_b are empty or can't be multiplied
+        TypeError: if m_a or m_b have non int/float or are not rect
+    """
+    m_a_notempty = True
+    m_b_notempty = True
+    m_a_rect = True
+    m_b_rect = True
+    m_a_num = True
+    m_b_num = True
+    m_a_notscalar = True
+    m_b_notscalar = True
+
+    for row in m_a:
+        if not isinstance(row, list):
+            m_a_notscalar = False
+        if len(row) != len(m_a[0]):
+            m_a_rect = False
+        for num in row:
+            if not isinstance(num, (int, float)):
+                m_a_num = False
+
+    for row in m_b:
+        if not isinstance(row, list):
+            m_b_notscalar = False
+        if len(row) != len(m_b[0]):
+            m_b_rect = False
+        for num in row:
+            if not isinstance(num, (int, float)):
+                m_b_num = False
+
+    if not m_a_notscalar:
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
+
+    if not m_b_notscalar:
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
+
+    if not m_a_num:
+        raise TypeError("invalid data type for einsum")
+
+    if not m_b_num:
+        raise TypeError("invalid data type for einsum")
+
+    if not m_a_rect:
+        raise ValueError("setting an array element with a sequence.")
+
+    if not m_b_rect:
+        raise ValueError("setting an array element with a sequence.")
+
+    return numpy.matrix(m_a) * numpy.matrix(m_b)
