d5d77453a26a682c0069f6f42fbd6a0b5c3f11c3
Hamid <hemidbeyli2020@gmail.com>
2024-02-04 06:42:03 -0800

    Bug fixed

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -38,7 +38,7 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b should contain only integers or floats")
 
     if any(len(row) != len(m_a[0]) for row in m_a[1:]):
-    raise ValueError("each row of m_a must be of the same size")
+        raise ValueError("each row of m_a must be of the same size")
 
     if any(len(row) != len(m_b[0]) for row in m_b[1:]):
         raise ValueError("each row of m_b must be of the same size")
