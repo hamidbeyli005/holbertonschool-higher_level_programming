b42f61ad49cef27ce5ba9ca82bc9167931f9e324
Hamid <hemidbeyli2020@gmail.com>
2024-02-04 08:07:40 -0800

    updated

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -19,11 +19,7 @@ def lazy_matrix_mul(m_a, m_b):
     Returns:
         list of lists: The result of the matrix multiplication.
     """
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-    if not m_a or any(not row for row in m_a):
+   if not m_a or any(not row for row in m_a):
         raise ValueError("m_a can't be empty")
     if not m_b or any(not row for row in m_b):
         raise ValueError("m_b can't be empty")
