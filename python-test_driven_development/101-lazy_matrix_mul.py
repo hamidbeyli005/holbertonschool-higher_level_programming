a5b8c35114e013401df2c9cc8b7beb937595c09b
Hamid <hemidbeyli2020@gmail.com>
2024-02-04 08:06:24 -0800

    updated

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -23,12 +23,6 @@ def lazy_matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-
-    if not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-
     if not m_a or any(not row for row in m_a):
         raise ValueError("m_a can't be empty")
     if not m_b or any(not row for row in m_b):
