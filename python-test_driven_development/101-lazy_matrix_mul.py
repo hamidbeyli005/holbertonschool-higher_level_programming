8b6d930be16d24ad222762fa15aab1616d7a9b8e
Hamid <hemidbeyli2020@gmail.com>
2024-02-04 08:15:06 -0800

    Updated

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -19,7 +19,17 @@ def lazy_matrix_mul(m_a, m_b):
     Returns:
         list of lists: The result of the matrix multiplication.
     """
-   if not m_a or any(not row for row in m_a):
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+    if not m_a or any(not row for row in m_a):
         raise ValueError("m_a can't be empty")
     if not m_b or any(not row for row in m_b):
         raise ValueError("m_b can't be empty")
@@ -34,8 +44,13 @@ def lazy_matrix_mul(m_a, m_b):
 
     if any(len(row) != len(m_b[0]) for row in m_b[1:]):
         raise TypeError("each row of m_b must be of the same size")
+
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    result = np.matmul(m_a, m_b)
+    try:
+        result = np.dot(m_a, m_b)
+    except ValueError:
+        raise ValueError("m_a and m_b can't be multiplied")
+
     return result
