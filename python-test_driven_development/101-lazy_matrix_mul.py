0373ab67bb0a7656e0a2696cfcc2241159ad19d2
Hamid <hemidbeyli2020@gmail.com>
2024-02-04 08:20:28 -0800

    Hot fix

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,53 +4,15 @@ lazy matrix multiplication using a module
 more experience with unit testing
 """
 
-
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """
-    Multiplies two matrices using NumPy.
+    """Return the multiplication of two matrices.
 
     Args:
-        m_a (list of lists): The first matrix.
-        m_b (list of lists): The second matrix.
-
-    Returns:
-        list of lists: The result of the matrix multiplication.
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
     """
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
 
-    if not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-
-    if not m_a or any(not row for row in m_a):
-        raise ValueError("m_a can't be empty")
-    if not m_b or any(not row for row in m_b):
-        raise ValueError("m_b can't be empty")
-
-    if any(not isinstance(elem, (int, float)) for row in m_a for elem in row):
-        raise TypeError("m_a should contain only integers or floats")
-    if any(not isinstance(elem, (int, float)) for row in m_b for elem in row):
-        raise TypeError("m_b should contain only integers or floats")
-
-    if any(len(row) != len(m_a[0]) for row in m_a[1:]):
-        raise TypeError("each row of m_a must be of the same size")
-
-    if any(len(row) != len(m_b[0]) for row in m_b[1:]):
-        raise TypeError("each row of m_b must be of the same size")
-
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-
-    try:
-        result = np.dot(m_a, m_b)
-    except ValueError:
-        raise ValueError("m_a and m_b can't be multiplied")
-
-    return result
+    return (np.matmul(m_a, m_b))
