13d30f913f746a80ce89e05856568c8797353f70
Hamid <hemidbeyli2020@gmail.com>
2024-02-05 11:39:00 -0800

    Added function documentation

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,9 +5,11 @@
 class BaseGeometry:
     """BaseGeometry class"""
     def area(self):
+        """area method"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
+        """integer validator module"""
         if type(value) != int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
