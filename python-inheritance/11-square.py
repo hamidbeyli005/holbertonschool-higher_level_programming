9f983579b1cabcf30f9e088e237fdf485201d758
Hamid <hemidbeyli2020@gmail.com>
2024-02-05 14:19:14 -0800

    Override __str__ method

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """My Square Class"""
+    def __init__(self, size):
+        """Init method"""
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Area method override"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Override str method"""
+        return '[Square] {}/{}'.format(self.__size, self.__size)
