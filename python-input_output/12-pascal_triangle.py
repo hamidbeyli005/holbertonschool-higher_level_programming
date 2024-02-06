b9e7f919c9f4f2e2db511c73ea8f3a091d6322f1
Hamid <hemidbeyli2020@gmail.com>
2024-02-06 06:02:19 -0800

    Updated pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,9 +6,10 @@ def pascal_triangle(n):
     """pascal function"""
     if n <= 0:
         return []
-    matrix = []
-
+    triangle = []
     for i in range(n):
-        row = str(11 ** i)
-        matrix.append(row)
-    return matrix
+        row = [1] * (i+1)
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        triangle.append(row)
+    return triangle
