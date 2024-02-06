076fa4d27ad238cd374254487d8a49c8acaf03b7
Hamid <hemidbeyli2020@gmail.com>
2024-02-06 02:39:56 -0800

    Added to_json method

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """init module"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """To json function"""
+        if attrs is None:
+            return vars(self)
+        else:
+            r = {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+            return r
