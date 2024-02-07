c231ba31a5cb879eccfe5612efc6d7b9b24b9751
Hamid <hemidbeyli2020@gmail.com>
2024-02-07 11:04:48 -0800

    Updated documentation 2

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -8,12 +8,8 @@ BaseGeometr = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle.
-    Private instance attributes:
-        - width
-        - height
-    Inherits from BaseGeometry.
-    """
+    """Rectangle class""
+
     def __init__(self, width, height):
         self.integer_validator('width', width)
         self.integer_validator('height', height)
