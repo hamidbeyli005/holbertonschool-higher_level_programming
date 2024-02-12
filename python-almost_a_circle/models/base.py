f963b65f376a072e07e1983e82eff3220c9d6d1c
Hamid <hemidbeyli2020@gmail.com>
2024-02-12 14:03:54 -0800

    Added from_json_string method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -32,3 +32,10 @@ class Base:
         filename = cls.__name__ + '.json'
         with open(filename, 'w', encoding='utf-8') as my_file:
             my_file.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ from json string method"""
+        if json_string is None or not json_string:
+            return []
+        return json.loads(json_string)
