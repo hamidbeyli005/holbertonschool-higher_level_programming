482de2353e44c258102446c58c98a707c3a2dc16
Hamid <hemidbeyli2020@gmail.com>
2024-02-11 23:51:56 -0800

    Added **kvargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -70,14 +70,18 @@ class Rectangle(Base):
     def __str__(self):
         return '[{}] ({}) {}/{} - {}/{}'.format(self.__class__.__name__, self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args):
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+    def update(self, *args, **kvargs):
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for key, value in kvargs.items():
+                setattr(self, key, value)
