4f5b1431d65be45148a5ae78a85cb762b8a86066
Hamid <hemidbeyli2020@gmail.com>
2024-02-11 23:29:40 -0800

    Added validate_integer

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,6 +19,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        self.validate_integer('width', value, False)
         self.__width = value
 
     @property
@@ -27,6 +28,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        self.validate_integer('height', value, False)
         self.__height = value
 
     @property
@@ -35,6 +37,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        self.validate_integer('x', value)
         self.__x = value
 
     @property
@@ -43,4 +46,14 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        self.validate_integer('y', value)
         self.__y = value
+
+    @staticmethod
+    def validate_integer(name, value, equal = True):
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer.'.format(name))
+        if equal and value < 0:
+            raise ValueError('{} must be > 0'.format(name))
+        elif not equal and value <= 0:
+            raise ValueError('{} must be >= 0'.format(name))
