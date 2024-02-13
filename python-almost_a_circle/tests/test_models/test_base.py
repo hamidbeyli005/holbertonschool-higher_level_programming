8974ee413949de16b8c9f41a34e052b14df6fcf9
Hamid <hemidbeyli2020@gmail.com>
2024-02-13 05:50:18 -0800

    Bug fixed

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -19,7 +19,7 @@ class TestBase(unittest.TestCase):
     def test_to_json_string(self):
         self.assertEqual(Base.to_json_string(None), '[]')
         self.assertEqual(Base.to_json_string([]), '[]')
-        self.assertEqual(Base.to_json_string([{'id': 12 }]), '[{'id': 12}]')
+        self.assertEqual(Base.to_json_string([{'id': 12 }]), '[{"id": 12}]')
 
     def test_from_json_string(self):
         self.assertEqual(Base.from_json_string(None), [])
