c3d70ea5e5755d210ad91143f4725065bba2393a
Hamid <hemidbeyli2020@gmail.com>
2024-02-13 04:30:49 -0800

    Created test file

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+"""Unit test"""
+
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+class TestBase(unittest.TestCase):
+
+    def test_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
