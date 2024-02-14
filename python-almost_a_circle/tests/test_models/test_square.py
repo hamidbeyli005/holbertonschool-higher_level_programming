4e2a36c90b80399c51489458cece48fd8b969d4a
Hamid <hemidbeyli2020@gmail.com>
2024-02-14 02:21:31 -0800

    Added new test case

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -4,7 +4,7 @@
 
 import unittest
 from models.square import Square
-
+import os
 
 class TestBase(unittest.TestCase):
     def test_square_positive_num(self):
@@ -49,4 +49,43 @@ class TestBase(unittest.TestCase):
         square.update(size=7)
         self.assertEqual(square.size, 7)
 
+    def test_create(self):
+        square = Square.create(**{ 'id': 89, 'size': 1, 'x': 2, 'y': 3 })
+        self.assertEqual(square.id, 89)
     
+    def test_save_to_file(self):
+        try:
+            os.remove("Square.json")
+        except:
+            pass
+        
+        Square.save_to_file(None)
+        with open("Square.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+
+
+        try:
+            os.remove("Square.json")
+        except:
+            pass
+
+        Square.save_to_file([])
+        with open("Square.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+
+
+        try:
+            os.remove("Square.json")
+        except:
+            pass
+
+        Square.save_to_file([Square(1, id=1)])
+        with open("Square.json", "r") as f:
+            self.assertEqual(f.read(), '[{"id": 1, "x": 0, "size": 1, "y": 0}]')
+
+    def test_load_from_file(self):
+        try:
+            os.remove("Square.json")
+        except:
+            pass
+        self.assertEqual(Square.load_from_file(), [])
