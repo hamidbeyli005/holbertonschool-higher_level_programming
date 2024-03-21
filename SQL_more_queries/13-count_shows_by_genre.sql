b84d9a72970acae11592022232a932a7fb7be09a
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-21 23:45:20 +0400

    Added count_shows_by_genre file

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_genres.name as genre, COUNT(show_id) as number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
