64fe82b4987c9ab4e60a079d8d096ccde77ebf0a
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-23 00:04:56 +0400

    Updated model_state file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,10 +4,11 @@
 """
 
 
-from sqlalchemy import Column,Integer,String 
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     """
