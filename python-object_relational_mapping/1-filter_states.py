2b323ec46205ce4a63e4007245f23c75d75efb13
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-22 19:45:00 +0400

    Added filter_states file

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""select_stateslists all states from the database hbtn_0e_0_usa"""
+"""filter_states lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db_connection = MySQLdb.connect(
+    db_connection = MySQLdb.Connect(
         host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
     cursor = db_connection.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    
     rows = cursor.fetchall()
-
     for row in rows:
         print(row)
+        
     cursor.close()
     db_connection.close()
