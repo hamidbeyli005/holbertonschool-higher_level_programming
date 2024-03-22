386aac77915e95dad30f165d3546a659f9fd5989
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-22 20:10:20 +0400

    Updated my_filter_states file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""displays all values in the states table of hbtn_0e_0_usa 
+"""
+    displays all values in the states table of hbtn_0e_0_usa
     where name matches the argument
 """
 
@@ -8,11 +9,13 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db_connection = MySQLdb.connect(host='localhost', port=3306, user=argv[1],passwd=argv[2],db=argv[3])
+    db_connection = MySQLdb.connect(host='localhost', port=3306,
+                                    user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db_connection.cursor()
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(argv[4])
+    query = """SELECT * FROM states WHERE name LIKE BINARY '{}'
+                ORDER BY id""".format(argv[4])
     cursor.execute(query)
-    
+
     rows = cursor.fetchall()
     for row in rows:
         print(row)
