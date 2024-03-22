afa7092747d662230bf70e1bf839bc5041e34d8a
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-23 00:43:05 +0400

    Updated model_state_fetch_first

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -19,4 +19,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).first()
-    print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
